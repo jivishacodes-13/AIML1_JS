82cd8daa01c5effd5bd36bc8e39d74b6b69adc6b
Jivisha Gupta <jivishagupta07@gmail.com>
2026-02-20 10:34:20 +0530

    java changes

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("Hello World!");
+// //.console.log("Hello World!");
+// /*var num = 10;
+// console.log(num);
+// console.log(typeof num);
+// var num = "Hello World!";
+// console.log(num);
+// console.log(typeof num); 
+// num = false;
+// console.log(num);
+// console.log(typeof num);
+// num = null;
+// console.log(num);
+// console.log(typeof num);
+// num = undefined;
+// console.log(num);
+// console.log(typeof num)
+// /**********Data Types in JavaScript**********/
+// const sym1 = Symbol(4);
+// console.log(sym1);
+// const sym2 = Symbol(4);
+// console.log(sym2);
+// if(sym1 === sym2){
+//     console.log("True");
+// }
+// else{    console.log("False");
+// }
+// /**********Objects in JavaScript**********/ 
+var a = 100; var b = 200; var c = 300; var linebreak ="<br>";
+document.write("a+b+c=");
+result =a+b+c;
+document.write(result);
+document.write(linebreak);
