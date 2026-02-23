b5225bd5eda32e80972e337aa883f2a13abb65d4
Jivisha Gupta <jivishagupta07@gmail.com>
2026-02-23 15:43:01 +0530

    New day new file

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- console.log("Hello World!");
+ /*console.log("Hello World!");
  var num = 10;
 console.log(num);
  console.log(typeof num);
@@ -15,7 +15,7 @@ console.log(num);
  console.log(num);
 console.log(typeof num)
  /**********Data Types in JavaScript**********/
- const sym1 = Symbol(4);
+/* const sym1 = Symbol(4);
 console.log(sym1);
 const sym2 = Symbol(4);
 console.log(sym2);
@@ -25,7 +25,7 @@ if(sym1 === sym2){
 else{    console.log("False");
 }
  /**********Objects in JavaScript**********/ 
-var a = 100; var b = 200; var c = 300; var linebreak ="<br>";
+/*var a = 100; var b = 200; var c = 300; var linebreak ="<br>";
 document.write("a+b+c=");
 result =a+b+c;
 document.write(result);
@@ -43,3 +43,46 @@ result =a%b%c;
 document.write(result); 
 document.write(linebreak);
 /**********Airthmatic operators in JavaScript**********/
+/*var a=10; var b=20; linebreak = "<br>";
+document.write("(a==b) = ");
+result = (a==b);
+document.write(result);
+document.write(linebreak);
+document.write("(a!=b) = ");
+result = (a!=b);
+document.write(result);     
+document.write(linebreak);
+document.write("(a>b) = ");
+result = (a>b);  
+document.write(result);
+document.write(linebreak);
+document.write("(a<b) = ");
+result = (a<b);         
+document.write(result); 
+document.write(linebreak);
+document.write("(a>=b) = ");
+result = (a>=b);
+document.write(result);
+document.write(linebreak);
+document.write("(a<=b) = ");
+result = (a<=b);  
+document.write(result);
+document.write(linebreak);
+/**********Relational operators in JavaScript**********/
+// write a program to find the largest of three numbers using ternary operator
+/*var count;
+document.write("Starting loop" +"<br>");
+for(count=1; count<=10; count++){
+    document.write("Current count = " + count + "<br>");
+}
+document.write("Loop ended");
+/**********Loops in JavaScript**********/
+const person ={
+    fname : "Omika",
+    lname : "Kumar",
+    age : 18,
+}
+for (let x in person){
+    console.log("Person's details  " + x + " is " + person[x]);
+}
+/**********For in loop in JavaScript**********/
