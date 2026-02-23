13b63c8816ef1f6ca2032568c9b53630ef6938a5
Jivisha Gupta <jivishagupta07@gmail.com>
2026-02-23 23:02:53 +0530

    Done

---
+++ assignment1.js
@@ -0,0 +1,28 @@
+//assignment operators in javascript
+var a = 10;
+var b = 20;
+var linebreak = "<br>";
+document.write("Value of a => (a=b) = ");
+result = (a=b);
+document.write(result);
+document.write(linebreak);
+document.write("Value of a => (a+=b) = ");
+result = (a+=b);
+document.write(result);
+document.write(linebreak);
+document.write("Value of a => (a-=b) = ");
+result = (a-=b);
+document.write(result);
+document.write(linebreak);
+document.write("Value of a => (a*=b) = ");
+result = (a*=b);
+document.write(result);
+document.write(linebreak);
+document.write("Value of a => (a/=b) = ");
+result = (a/=b);
+document.write(result);
+document.write(linebreak);
+document.write("Value of a => (a%=b) = ");
+result = (a%=b);
+document.write(result); 
+document.write(linebreak);
